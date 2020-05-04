057748a3d73a6e8346800bb0d3900bd4eda9ca43
Paul Solt <PaulSolt@gmail.com>
2020-05-04 13:44:25 -0400

    Added filter function for CIColorControls Connected UI to filter a starter image

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,19 +1,78 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
 
+    let context = CIContext(options: nil)
+    
+    var originalImage: UIImage? {
+        didSet {
+            updateViews()
+        }
+    }
+    
 	@IBOutlet weak var brightnessSlider: UISlider!
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-	}
+        let filter = CIFilter(name: "CIColorControls")! // built-in filter from Apple
+        print(filter)
+        print(filter.attributes)
+        
+        // Demo with a starter image from storyboard
+        originalImage = imageView.image
+    }
 	
+    // filterImage(originalImage)
+
+    // Create a stub with default return is a good way to start
+    private func filterImage(_ image: UIImage) -> UIImage? {
+        
+        // UIImage -> CGImage -> CIImage
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        // TODO: show builtins filters
+        
+        // Filter image
+        let filter = CIFilter(name: "CIColorControls")! // built-in filter from Apple
+//        let filter2 = CIFilter.colorControls()
+//        filter2.brightness = brightnessSlider.value
+        
+        // setting values / gettings values from Core Image
+        filter.setValue(ciImage, forKey: kCIInputImageKey) //"inputImage")
+        filter.setValue(saturationSlider.value, forKey: kCIInputSaturationKey) // k = constant in Objective-C
+        filter.setValue(brightnessSlider.value, forKey: kCIInputBrightnessKey)
+        filter.setValue(contrastSlider.value, forKey: kCIInputContrastKey)
+        
+        // CIImage -> CGImage -> UIImage
+        
+        // guard let outputCIImage = filter.value(forKey: kCIOutputImageKey) as? CIImage else { return nil }
+        guard let outputCIImage = filter.outputImage else { return nil }
+        
+        // Render the image (do image processing here)
+        guard let outputCGImage = context.createCGImage(outputCIImage,
+                                                        from: CGRect(origin: .zero, size: image.size)) else {
+            return nil
+        }
+        
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    private func updateViews() {
+        if let originalImage = originalImage {
+            imageView.image = filterImage(originalImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
 	// MARK: Actions
 	
 	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
@@ -28,16 +87,16 @@ class PhotoFilterViewController: UIViewController {
 
 	// MARK: Slider events
 	
-	@IBAction func brightnessChanged(_ sender: UISlider) {
+    @IBAction func brightnessChanged(_ sender: UISlider) {
+        updateViews()
+    }
 
-	}
-	
-	@IBAction func contrastChanged(_ sender: Any) {
+    @IBAction func contrastChanged(_ sender: Any) {
+        updateViews()
+    }
 
-	}
-	
-	@IBAction func saturationChanged(_ sender: Any) {
-
-	}
+    @IBAction func saturationChanged(_ sender: Any) {
+        updateViews()
+    }
 }
 
